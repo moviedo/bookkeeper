8200519308b00433dde97871bff3a5dc24c2ce8a
Mauro Oviedo <moviedo@arsenal.com>
2022-07-06 10:16:53 -0400

    fix: default id logic

--- apps/bookkeeper-api/database/migrations/1656616554993_users.ts
+++ apps/bookkeeper-api/database/migrations/1656616554993_users.ts
@@ -5,7 +5,7 @@ export default class UsersSchema extends BaseSchema {
 
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.uuid('id').primary()
+      table.uuid('id').primary().defaultTo(this.raw('uuid_generate_v4()'))
 
       table.string('username', 255).unique().notNullable()
       table.string('email', 255).unique().notNullable()
