ff35e13bc24f15f94f8436eef3a7a373a32427de
Mauro Oviedo <moviedo@arsenal.com>
2022-07-14 12:05:57 -0400

    build: tailwind install/config

--- apps/bookkeeper-ui/nuxt.config.js
+++ apps/bookkeeper-ui/nuxt.config.js
@@ -21,7 +21,7 @@ export default {
   },
 
   // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [],
+  css: ["@/assets/css/main.css"],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [],
@@ -31,6 +31,8 @@ export default {
 
   // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
   buildModules: [
+    // https://github.com/nuxt/postcss8
+    "@nuxt/postcss8",
     // https://go.nuxtjs.dev/typescript
     "@nuxt/typescript-build",
   ],
@@ -45,5 +47,12 @@ export default {
   axios: {},
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
+  build: {
+    postcss: {
+      plugins: {
+        tailwindcss: {},
+        autoprefixer: {},
+      },
+    },
+  },
 };
