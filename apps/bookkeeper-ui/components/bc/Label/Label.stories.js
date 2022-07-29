0d19b2424fdb80c3531b1be187f4a9e5936b066d
Mauro Oviedo <moviedo@arsenal.com>
2022-07-29 13:34:53 -0400

    feat: bc-label component

---
+++ apps/bookkeeper-ui/components/bc/Label/Label.stories.js
@@ -0,0 +1,7 @@
+export default {
+  title: "base components / bc-label",
+};
+
+export const Default = () => ({
+  template: "<bc-label>Input Label</bc-label>",
+});
---
+++ apps/bookkeeper-ui/components/bc/Label/Label.vue
@@ -0,0 +1,12 @@
+<template>
+  <label class="text-base font-normal text-slate-600">
+    <slot />
+  </label>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+export default Vue.extend({
+  name: "BcLabel",
+});
+</script>
