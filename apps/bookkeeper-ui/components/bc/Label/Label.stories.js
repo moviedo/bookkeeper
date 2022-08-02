0c919b1bcaadd9854ac8acb9b77d33a969219545
Mauro Oviedo <moviedo@arsenal.com>
2022-08-02 15:33:02 -0400

    test: add WithCustomHtml for bc-label

--- apps/bookkeeper-ui/components/bc/Label/Label.stories.js
+++ apps/bookkeeper-ui/components/bc/Label/Label.stories.js
@@ -2,12 +2,17 @@ export default {
   title: "base components / bc-label",
 };
 
-const Template = (_args, { argTypes }) => ({
+const Template = (args, { argTypes }) => ({
   props: Object.keys(argTypes),
-  template: "<bc-label>{{$props.slot}}</bc-label>",
+  template: `<bc-label>${args.default}</bc-label>`,
 });
 
 export const Default = Template.bind({});
 Default.args = {
-  slot: "Input Label",
+  default: "Input Label",
+};
+
+export const WithCustomHtml = Template.bind({});
+WithCustomHtml.args = {
+  default: "<strong>Strong Label</strong>: with normal label",
 };
